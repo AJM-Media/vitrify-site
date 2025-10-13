7cccb1b5e94b701f737025e8f9d6363989c3c074
Andrew Murray <hello@ajmmedia.co.uk>
2025-10-13 16:58:46 +0100

    Add better comments to show that auto escape is false by default; more comments to StarterSite class and views directory

--- functions.php
+++ functions.php
@@ -22,6 +22,17 @@ if ( ! class_exists( 'Timber' ) ) {
 
 Timber::$dirname = array('templates', 'views');
 
+/**
+ * By default, Timber does NOT autoescape values. Want to enable Twig's autoescape? 
+ * No prob! Just set this value to true
+ */
+Timber::$autoescape = false;
+
+
+/**
+ * We're going to configure our theme inside of a subclass of Timber\Site
+ * You can move this to its own file and include here via php's include("MySite.php")
+ */
 class StarterSite extends Timber\Site {
 
 	function __construct() {
