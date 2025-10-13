6074326ed82ac57bb48628bde4a3ccb4f760d675
Andrew Murray <hello@ajmmedia.co.uk>
2025-10-13 16:58:55 +0100

    feat: rework of PHP files. (#152)
    * feat: rework of PHP files.
    * chore: update namespace
    * fix: don't provide post again
    * Remove $dirname since we only use the default location now.
    * chore: remove unneeded get_post(s) calls

--- functions.php
+++ functions.php
@@ -1,12 +1,18 @@
 <?php
+
 /**
- * Timber starter-theme
- * https://github.com/timber/starter-theme
+ * Functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ * @link https://github.com/timber/starter-theme
  */
+
+namespace App;
+
+use Timber\Timber;
 
 // Load Composer dependencies.
 require_once __DIR__ . '/vendor/autoload.php';
-
 require_once __DIR__ . '/src/StarterSite.php';
 
 Timber\Timber::init();
