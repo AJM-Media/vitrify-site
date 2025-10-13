e9fead7cb182d393af45992e6da8626d3c3fe67a
Andrew Murray <hello@ajmmedia.co.uk>
2025-10-13 16:58:54 +0100

    Autoload StarterSite class through Composer

--- src/StarterSite.php
+++ src/StarterSite.php
@@ -1,50 +1,37 @@
 <?php
-/**
- * Timber starter-theme
- * https://github.com/timber/starter-theme
- */
-
-// Load Composer dependencies.
-require_once __DIR__ . '/vendor/autoload.php';
-
-Timber\Timber::init();
-
-Timber::$dirname = array('templates', 'views');
 
 /**
- * By default, Timber does NOT autoescape values. Want to enable Twig's autoescape?
- * No prob! Just set this value to true
- */
-Timber::$autoescape = false;
-
-
-/**
- * We're going to configure our theme inside of a subclass of Timber\Site
- * You can move this to its own file and include here via php's include("MySite.php")
+ * Class StarterSite
  */
 class StarterSite extends Timber\Site {
-
-	function __construct() {
-		add_theme_support( 'post-formats' );
-		add_theme_support( 'post-thumbnails' );
-		add_theme_support( 'title-tag' );
-		add_theme_support( 'menus' );
-		add_filter( 'timber_context', array( $this, 'add_to_context' ) );
-		add_filter( 'get_twig', array( $this, 'add_to_twig' ) );
+	public function __construct() {
+		add_action( 'after_setup_theme', array( $this, 'theme_supports' ) );
 		add_action( 'init', array( $this, 'register_post_types' ) );
 		add_action( 'init', array( $this, 'register_taxonomies' ) );
+
+		add_filter( 'timber/context', array( $this, 'add_to_context' ) );
+		add_filter( 'timber/twig', array( $this, 'add_to_twig' ) );
+		add_filter( 'timber/twig/environment/options', [ $this, 'update_twig_environment_options' ] );
+
 		parent::__construct();
 	}
-	/** This is where you can register custom post types. */
+
+	/**
+	 * This is where you can register custom post types.
+	 */
 	public function register_post_types() {
 
 	}
-	/** This is where you can register custom taxonomies. */
+
+	/**
+	 * This is where you can register custom taxonomies.
+	 */
 	public function register_taxonomies() {
 
 	}
 
-	/** This is where you add some context
+	/**
+	 * This is where you add some context
 	 *
 	 * @param string $context context['this'] Being the Twig's {{ this }}.
 	 */
@@ -54,6 +41,7 @@ class StarterSite extends Timber\Site {
 		$context['notes'] = 'These values are available everytime you call Timber::context();';
 		$context['menu']  = Timber::get_menu();
 		$context['site']  = $this;
+
 		return $context;
 	}
 
@@ -111,7 +99,8 @@ class StarterSite extends Timber\Site {
 		add_theme_support( 'menus' );
 	}
 
-	/** This Would return 'foo bar!'.
+	/**
+	 * his would return 'foo bar!'.
 	 *
 	 * @param string $text being 'foo', then returned 'foo bar!'.
 	 */
@@ -120,16 +109,35 @@ class StarterSite extends Timber\Site {
 		return $text;
 	}
 
-	/** This is where you can add your own functions to twig.
+	/**
+	 * This is where you can add your own functions to twig.
 	 *
-	 * @param string $twig get extension.
+	 * @param Twig\Environment $twig get extension.
 	 */
 	public function add_to_twig( $twig ) {
-		$twig->addExtension( new Twig_Extension_StringLoader() );
-		$twig->addFilter(new Twig_SimpleFilter('myfoo', array($this, 'myfoo')));
+		/**
+		 * Required when you want to use Twig’s template_from_string.
+		 * @link https://twig.symfony.com/doc/3.x/functions/template_from_string.html
+		 */
+		// $twig->addExtension( new Twig\Extension\StringLoaderExtension() );
+
+		$twig->addFilter( new Twig\TwigFilter( 'myfoo', [ $this, 'myfoo' ] ) );
+
 		return $twig;
 	}
 
-}
+	/**
+	 * Updates Twig environment options.
+	 *
+	 * @link https://twig.symfony.com/doc/2.x/api.html#environment-options
+	 *
+	 * \@param array $options An array of environment options.
+	 *
+	 * @return array
+	 */
+	function update_twig_environment_options( $options ) {
+	    // $options['autoescape'] = true;
 
-new StarterSite();
+	    return $options;
+	}
+}
